a65e907f9d6eaf4b095933e8399604fd814ceacd
Abhijit7-pix <130569134+Abhijit7-pix@users.noreply.github.com>
2025-07-24 17:58:37 +0530

    some new

--- Home.js
+++ Home.js
@@ -1,3 +1,10 @@
-<script>
-
-</script>
+// function enlargeLogo() {
+//     const logo = document.getElementById('logo');
+//     logo.classList.toggle('enlarged');
+// }
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        const logo = document.getElementById('logo');
+        logo.classList.add('visible');
+    }, 3000); // Show after 3 seconds
+});
